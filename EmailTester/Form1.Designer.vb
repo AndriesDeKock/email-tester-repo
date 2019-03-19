c1c4efc98bcaaf5e1ab56ca3d420ae2d99c27216
AndriesDeKock <andries@snowblu.com>
2019-03-19 07:34:39 +0200

    Add project files.

---
+++ EmailTester/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFrom = New System.Windows.Forms.TextBox()
+        Me.txtTo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSMTP = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSMTPuser = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSMTPPass = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.chkSSL = New System.Windows.Forms.CheckBox()
+        Me.txtDomain = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "From:"
+        '
+        'txtFrom
+        '
+        Me.txtFrom.Location = New System.Drawing.Point(97, 139)
+        Me.txtFrom.Name = "txtFrom"
+        Me.txtFrom.Size = New System.Drawing.Size(184, 20)
+        Me.txtFrom.TabIndex = 1
+        Me.txtFrom.Text = "invoices@immploy.com"
+        '
+        'txtTo
+        '
+        Me.txtTo.Location = New System.Drawing.Point(97, 165)
+        Me.txtTo.Name = "txtTo"
+        Me.txtTo.Size = New System.Drawing.Size(184, 20)
+        Me.txtTo.TabIndex = 3
+        Me.txtTo.Text = "andries.de@gmail.com"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "To:"
+        '
+        'txtSMTP
+        '
+        Me.txtSMTP.Location = New System.Drawing.Point(97, 16)
+        Me.txtSMTP.Name = "txtSMTP"
+        Me.txtSMTP.Size = New System.Drawing.Size(184, 20)
+        Me.txtSMTP.TabIndex = 5
+        Me.txtSMTP.Text = "remote.immploy.biz"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "SMTP Server:"
+        '
+        'txtSMTPuser
+        '
+        Me.txtSMTPuser.Location = New System.Drawing.Point(97, 42)
+        Me.txtSMTPuser.Name = "txtSMTPuser"
+        Me.txtSMTPuser.Size = New System.Drawing.Size(184, 20)
+        Me.txtSMTPuser.TabIndex = 7
+        Me.txtSMTPuser.Text = "invoices"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "SMTP User:"
+        '
+        'txtSMTPPass
+        '
+        Me.txtSMTPPass.Location = New System.Drawing.Point(97, 68)
+        Me.txtSMTPPass.Name = "txtSMTPPass"
+        Me.txtSMTPPass.Size = New System.Drawing.Size(184, 20)
+        Me.txtSMTPPass.TabIndex = 9
+        Me.txtSMTPPass.Text = "&wBxhC^Q6w-4#zhP"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 71)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "SMTP Pass:"
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(206, 221)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 10
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(5, 193)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 13)
+        Me.lblStatus.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(21, 194)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(30, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "SSL:"
+        '
+        'chkSSL
+        '
+        Me.chkSSL.AutoSize = True
+        Me.chkSSL.Location = New System.Drawing.Point(97, 193)
+        Me.chkSSL.Name = "chkSSL"
+        Me.chkSSL.Size = New System.Drawing.Size(82, 17)
+        Me.chkSSL.TabIndex = 13
+        Me.chkSSL.Text = "Enable SSL"
+        Me.chkSSL.UseVisualStyleBackColor = True
+        '
+        'txtDomain
+        '
+        Me.txtDomain.Location = New System.Drawing.Point(97, 94)
+        Me.txtDomain.Name = "txtDomain"
+        Me.txtDomain.Size = New System.Drawing.Size(184, 20)
+        Me.txtDomain.TabIndex = 15
+        Me.txtDomain.Text = "IMMPLOY"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(17, 97)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(46, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Domain:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(290, 250)
+        Me.Controls.Add(Me.txtDomain)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.chkSSL)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtSMTPPass)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSMTPuser)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtSMTP)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtTo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtFrom)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtFrom As System.Windows.Forms.TextBox
+    Friend WithEvents txtTo As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSMTP As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtSMTPuser As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtSMTPPass As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents chkSSL As System.Windows.Forms.CheckBox
+    Friend WithEvents txtDomain As TextBox
+    Friend WithEvents Label7 As Label
+End Class
---
+++ EmailTester/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+
+    Private Sub btnSend_Click(sender As System.Object, e As System.EventArgs) Handles btnSend.Click
+
+        'Dim obj() As Object = {txtFrom, txtSMTP, txtSMTPPass, txtSMTPuser, txtTo}
+        'For i As Integer = 0 To obj.Length - 1
+        '    If obj(i).text = "" Then
+        '        MessageBox.Show("One of more fields are empty.", "Empty", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        '        Exit Sub
+        '    End If
+        'Next
+
+        lblStatus.Text = "Sending email..."
+        Application.DoEvents()
+
+        Dim strResults As String = f_sendAutoEmailstr(txtTo.Text, txtFrom.Text, "",
+                                                      "Immploy Tax Invoice", _RetrieveEmailBody(), "", txtSMTP.Text,
+                                                      txtSMTPuser.Text, txtSMTPPass.Text, chkSSL.Checked, txtDomain.Text)
+        If strResults <> "" Then
+            lblStatus.Text = ""
+            Application.DoEvents()
+            MessageBox.Show("There was an error - Error: " & strResults, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            lblStatus.Text = "Email was sent!"
+            Application.DoEvents()
+        End If
+
+    End Sub
+
+#Region "Functions"
+
+    Private Function _RetrieveEmailBody()
+
+        Dim span As String = "<span lang='en'>"
+        Dim body As String = "<BODY style=""font-size:11pt;font-family:Calibri"">"
+        Dim body_close As String = "</BODY>"
+        Dim span_close As String = "</span>"
+        Dim email As String = "<a href=""mailto:finance@immploy.com"">finance@immploy.com</a>"
+        Dim img As String = "<img src=""\\atomicx\1 Documents\Images\immploy_finance.png"">"
+
+        Dim strContent As String = "Good day <NAME><br/><br/>Attached please find our invoice for services provided by Immploy Recruitment Agency.<br/><br/>If you are in agreement, kindly settle the invoice at your earliest convenience but no later than 30 days from date of invoice. If you need to query the invoice, kindly do so within 7 days of receipt of the email.<br/><br/>Please direct all enquiries to " & email & ".<br/><br/>Regards<br/><br/>" & img
+
+
+        Return strContent
+
+    End Function
+
+    Private Function f_sendAutoEmailstr(ByVal toAdres As String, ByVal fromAdres As String,
+                                        ByVal ccAdres As String, ByVal subject As String,
+                                        ByVal body As String, ByVal attachmentPath As String,
+                                        ByVal smtpServer As String, ByVal smtpUsername As String,
+                                        ByVal smtpPassword As String, blSSL As Boolean,
+                                        Optional strDomain As String = "") As String
+
+        Dim strReturn As String = ""
+        Try
+            'create a new e-mail message
+            Dim oMsg As New System.Net.Mail.MailMessage(fromAdres, toAdres, subject, body)
+
+            'add the cc address if one is specified
+            If Not ccAdres = "" Then
+                oMsg.CC.Add(ccAdres)
+            End If
+
+            'add the attachment if one is specified
+            If Not attachmentPath = "" Then
+                Dim oAttch As Attachment = New Attachment(attachmentPath)
+                oMsg.Attachments.Add(oAttch)
+            End If
+
+            'create the SMTP client with the mail server name or IP
+            Dim smtpmail As New SmtpClient(smtpServer)
+
+            oMsg.IsBodyHtml = True
+
+            'if a user name and password is specified, add the credentials
+            If Not smtpUsername = "" Then
+                Dim smtpUser As New System.Net.NetworkCredential
+
+                smtpUser.UserName = smtpUsername
+                smtpUser.Password = smtpPassword
+
+                'log
+                Debug.WriteLine(String.Format("SMTP user: {0}, SMTP Pass: {1}", smtpUsername, smtpPassword))
+
+                If strDomain <> "" Then
+                    Debug.WriteLine(String.Format("Domain: {0}", strDomain))
+                    smtpUser.Domain = strDomain
+                End If
+
+                smtpmail.EnableSsl = blSSL
+                smtpmail.Port = 9925
+
+
+                Debug.WriteLine(String.Format("SSL Enabled: {0}, Port: {1}", blSSL.ToString(), smtpmail.Port.ToString()))
+
+                smtpmail.UseDefaultCredentials = False
+                Debug.WriteLine(String.Format("Use Default Credentials: {0}", smtpmail.UseDefaultCredentials.ToString()))
+
+                smtpmail.Credentials = smtpUser
+            End If
+
+            'send the e-mail
+            smtpmail.Send(oMsg)
+
+        Catch ex As Exception
+            Debug.WriteLine(String.Format("Error: {0}", ex.Message.ToString()))
+            strReturn = ex.Message
+        End Try
+
+        Return strReturn
+
+    End Function
+
+#End Region
+
+
+
+End Class
