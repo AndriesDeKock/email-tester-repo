24e32512676223ff3f5bbc10dbe7f6b3c519f5bd
AndriesDeKock <andries@snowblu.com>
2019-05-07 14:48:53 +0200

    Updated email message body

--- EmailTester/Form1.vb
+++ EmailTester/Form1.vb
@@ -4,20 +4,10 @@ Public Class Form1
 
     Private Sub btnSend_Click(sender As System.Object, e As System.EventArgs) Handles btnSend.Click
 
-        'Dim obj() As Object = {txtFrom, txtSMTP, txtSMTPPass, txtSMTPuser, txtTo}
-        'For i As Integer = 0 To obj.Length - 1
-        '    If obj(i).text = "" Then
-        '        MessageBox.Show("One of more fields are empty.", "Empty", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        '        Exit Sub
-        '    End If
-        'Next
-
         lblStatus.Text = "Sending email..."
         Application.DoEvents()
 
-
-        Dim strResults As String = f_sendAutoEmailstr(txtTo.Text, txtFrom.Text, "",
-                                                      "Immploy Tax Invoice", _RetrieveEmailBody(), "", txtSMTP.Text,
+        Dim strResults As String = f_sendAutoEmailstr(txtTo.Text, txtFrom.Text, "", "Email Message", _RetrieveEmailBody(), "", txtSMTP.Text,
                                                       txtSMTPuser.Text, txtSMTPPass.Text, chkSSL.Checked, txtDomain.Text)
         If strResults <> "" Then
             lblStatus.Text = ""
@@ -38,10 +28,8 @@ Public Class Form1
         Dim body As String = "<BODY style=""font-size:11pt;font-family:Calibri"">"
         Dim body_close As String = "</BODY>"
         Dim span_close As String = "</span>"
-        Dim email As String = "<a href=""mailto:finance@immploy.com"">finance@immploy.com</a>"
-        Dim img As String = "<img src=""\\atomicx\1 Documents\Images\immploy_finance.png"">"
 
-        Dim strContent As String = "Good day <NAME><br/><br/>Attached please find our invoice for services provided by Immploy Recruitment Agency.<br/><br/>If you are in agreement, kindly settle the invoice at your earliest convenience but no later than 30 days from date of invoice. If you need to query the invoice, kindly do so within 7 days of receipt of the email.<br/><br/>Please direct all enquiries to " & email & ".<br/><br/>Regards<br/><br/>" & img
+        Dim strContent As String = "Good day <NAME><br/><br/>This is an email message.<br/><br/>"
 
 
         Return strContent
@@ -78,10 +66,10 @@ Public Class Form1
 
             'if a user name and password is specified, add the credentials
             If Not smtpUsername = "" Then
-                Dim smtpUser As New System.Net.NetworkCredential
-
-                smtpUser.UserName = smtpUsername
-                smtpUser.Password = smtpPassword
+                Dim smtpUser As New System.Net.NetworkCredential With {
+                    .UserName = smtpUsername,
+                    .Password = smtpPassword
+                }
 
                 'log
                 Debug.WriteLine(String.Format("SMTP user: {0}, SMTP Pass: {1}", smtpUsername, smtpPassword))
